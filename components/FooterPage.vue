1d6c419e95299cc344f744282c95ff5b46e11f4a
Diego Orellana <dev@dorellango.cl>
2018-06-04 15:31:49 -0400

    fix dorellango credits link

--- components/FooterPage.vue
+++ components/FooterPage.vue
@@ -33,7 +33,7 @@
         </div>
     </div>
     <p class="mt-6 text-grey-dark tracking-wide text-xs text-center border-t border-dashed border-grey-darker pt-6">
-        Designed by <a href="http://wwww.dorellango.cl" class="text-orange" target="_blank">@dorellango</a>
+        Designed by <a href="http://www.dorellango.cl" class="text-orange" target="_blank">@dorellango</a>
     </p>
     <modal name="address" height="auto" :adaptive="true">
         <div>
